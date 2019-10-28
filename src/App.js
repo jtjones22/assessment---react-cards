7fbdcd0be9ff7349a63984c863045aaa86e7544a
Jonathan Jones <jonathan.jones51@yahoo.com>
2019-10-28 10:48:19 -0400

    Removed the big bulk of html in the App and replaced it with the CardGroup and Card components.

--- src/App.js
+++ src/App.js
@@ -19,43 +19,57 @@ const CardGroup = (props) => (
 );
 
 const Card = (props) => (
-    <div>TODO: Implement Card Component</div>
+    <div className="card cardGroup__card">
+        <div className="card__description cardGroup__cardDescription">
+            <div className={`icon fa ${props.icon} card__descriptionIcon`} />
+            <div className="card__descriptionText">
+                {props.description}
+                {props.hint && <span><br/>{props.hint}</span>}
+            </div>
+        </div>
+        <div className="card__price">{props.price}!</div>
+    </div>
 );
 
 const App = () => (
-    <div className="cardGroup">
-        <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-thumbs-o-up card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Trial
-                </div>
-            </div>
-            <div className="card__price">Free!</div>
-        </div>
-        <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-trophy card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Basic tier
-                    <br/>
-                    (most popular)
-                </div>
-            </div>
-            <div className="card__price">$10.00</div>
-        </div>
-        <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-bolt card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Advanced tier
-                    <br/>
-                    (only for enterprise-level professionals)
-                </div>
-            </div>
-            <div className="card__price">$6,000.00</div>
-        </div>
-    </div>
+    <CardGroup>
+        <Card description="Trial" icon="fa-thumbs-o-up" price="Free" />
+        <Card description="Basic tier" hint="(most popular)" icon="fa-trophy" price="$10.00" />
+        <Card description="Advanced tier" hint="(only for enterprise-level professionals)" icon="fa-bolt" price="$6,000" />
+    </CardGroup>
+    // <div className="cardGroup">
+    //     <div className="card cardGroup__card">
+    //         <div className="card__description cardGroup__cardDescription">
+    //             <div className="icon fa fa-thumbs-o-up card__descriptionIcon" />
+    //             <div className="card__descriptionText">
+    //                 Trial
+    //             </div>
+    //         </div>
+    //         <div className="card__price">Free!</div>
+    //     </div>
+    //     <div className="card cardGroup__card">
+    //         <div className="card__description cardGroup__cardDescription">
+    //             <div className="icon fa fa-trophy card__descriptionIcon" />
+    //             <div className="card__descriptionText">
+    //                 Basic tier
+    //                 <br/>
+    //                 (most popular)
+    //             </div>
+    //         </div>
+    //         <div className="card__price">$10.00</div>
+    //     </div>
+    //     <div className="card cardGroup__card">
+    //         <div className="card__description cardGroup__cardDescription">
+    //             <div className="icon fa fa-bolt card__descriptionIcon" />
+    //             <div className="card__descriptionText">
+    //                 Advanced tier
+    //                 <br/>
+    //                 (only for enterprise-level professionals)
+    //             </div>
+    //         </div>
+    //         <div className="card__price">$6,000.00</div>
+    //     </div>
+    // </div>
 );
 
 export default App;
